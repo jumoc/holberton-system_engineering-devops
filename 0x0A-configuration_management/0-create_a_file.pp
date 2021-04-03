90da451a8b5c76f25b3429cf2f2d1fd21dccf9f4
Jumoc <juanpcaicedo2@gmail.com>
2021-04-03 00:53:41 -0500

    Using puppet created a file in a determined location

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Creates a file in the tmp folder
+
+file {'/tmp/holberton':
+  ensure  => file,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',
+}

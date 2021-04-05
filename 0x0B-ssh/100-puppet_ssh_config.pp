ba9dac2d8da33f82f81440919b7f4f32a83856a9
Jumoc <juanpcaicedo2@gmail.com>
2021-04-04 19:32:23 -0500

    Fix config with puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,10 @@
 # Setup config file
 file_line { 'config_password':
-  ensure => present,
   path   => '/etc/ssh/ssh_config',
-  line   => 'PasswordAuthentication no',
+  line   => 'BatchMode yes',
 }
 
 file_line { 'config_key':
-  ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile "~/.ssh/holberton"',
 }

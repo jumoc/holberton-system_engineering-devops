3050520df353846574d65d060cc2a3c14e57f838
Jumoc <juanpcaicedo2@gmail.com>
2021-04-04 19:45:51 -0500

    Change file_line titles to be exactly the ones that holberton used

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,11 +1,11 @@
 # Setup config file
-file_line { 'config_password':
+file_line { 'Turn off passwd auth':
   ensure => 'present'
   path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
 }
 
-file_line { 'config_key':
+file_line { 'Declare identity file':
   ensure => 'present'
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/holberton',

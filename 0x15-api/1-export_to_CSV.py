042cc677762a1cc87e7f209c701bccf9563130ff
Jumoc <juanpcaicedo2@gmail.com>
2021-05-03 23:05:56 -0500

    export to csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""gather data from an api module"""
+import requests
+import sys
+import os
+
+
+def get_data():
+    id = sys.argv[1]
+
+    user = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(id)
+        ).json()
+    todos = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
+        ).json()
+
+    text = ""
+    for todo in todos:
+        text += '"{}","{}","{}","{}"\n'.format(
+            user['id'], user['name'], todo['completed'], todo['title']
+        )
+
+    with open("{}.csv".format(id), "w") as file:
+        file.write(text)
+
+
+if __name__ == '__main__':
+    get_data()

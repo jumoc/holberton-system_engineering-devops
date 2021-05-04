6f2d53220e7fef2925e6c9627ac9ab1378ff608c
Jumoc <juanpcaicedo2@gmail.com>
2021-05-03 22:38:16 -0500

    Add (README, script): gather data from an api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""gather data from an api module"""
+import requests
+import sys
+
+
+def get_data():
+    id = sys.argv[1]
+    user = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(
+            id
+            )
+        )
+    todos = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}/todos".format(
+            id
+            )
+        )
+
+    comp = 0
+    for todo in todos.json():
+        if todo['completed'] is True:
+            comp += 1
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.json()['name'], comp, len(todos.json())
+    ))
+
+    for todo in todos.json():
+        print("\t {}".format(todo['title']))
+
+
+if __name__ == '__main__':
+    get_data()

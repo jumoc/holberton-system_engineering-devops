13a02c40456844646c2975a85e32ccfd4bde2c4c
Jumoc <juanpcaicedo2@gmail.com>
2021-05-03 23:23:01 -0500

    Add script to export to json

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""gather data from an api module"""
+import requests
+import sys
+import json
+
+
+def get_data():
+    """exports to csv"""
+    id = sys.argv[1]
+
+    user = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(id)
+        ).json()
+
+    todos = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
+        ).json()
+
+    tasks = []
+    for todo in todos:
+        tasks.append(
+            {"task": todo['title'], "completed": todo['completed'],
+             "username": user['username']}
+        )
+
+    n_dict = {user['id']: tasks}
+
+    with open("{}.json".format(id), "w") as file:
+        file.write(json.dumps(n_dict))
+
+
+if __name__ == '__main__':
+    get_data()
